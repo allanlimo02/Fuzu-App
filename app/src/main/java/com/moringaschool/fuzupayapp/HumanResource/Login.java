836d75042b72eb82d7bfd52f6432d5972fc8153e
DeveloperOwala <otienoerick46@gmail.com>
2021-11-11 12:37:26 +0300

    creating log in activity

---
+++ app/src/main/java/com/moringaschool/fuzupayapp/HumanResource/Login.java
@@ -0,0 +1,16 @@
+package com.moringaschool.fuzupayapp.HumanResource;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.moringaschool.fuzupayapp.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}

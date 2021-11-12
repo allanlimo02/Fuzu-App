d65ab3dddc26b8118e02e51053d58ac36af051e2
lanarokip <lankipkoecharo@gmail.com>
2021-11-12 13:27:42 +0300

    added the  tablayout facilitating the access to other adapters

---
+++ app/src/main/java/com/moringaschool/fuzupayapp/HumanResource/Dashboard/Dashboard.java
@@ -0,0 +1,16 @@
+package com.moringaschool.fuzupayapp.HumanResource.Dashboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.moringaschool.fuzupayapp.R;
+
+public class Dashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+}

8afd6cefe6a148ae132275a85cb298f366a88566
allanlimo02 <allan.limo@student.moringaschool.com>
2021-11-16 02:46:17 +0300

    Mondays Final push

--- app/src/main/java/com/moringaschool/fuzupayapp/HumanResource/Fragments/Staff/DepartmentsFragment.java
+++ app/src/main/java/com/moringaschool/fuzupayapp/HumanResource/Fragments/Staff/DepartmentsFragment.java
@@ -7,23 +7,30 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.moringaschool.fuzupayapp.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 
 public class DepartmentsFragment extends Fragment {
+//    Spinner spinner;
+    private Spinner spinner;
+
+//    @BindView(R.id.depspinner) Spinner depspinner;
 
 
     public DepartmentsFragment() {
         // Required empty public constructor
     }
-
-
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_departments, container, false);
-         return view;
+             return view;
+
     }
+
 }

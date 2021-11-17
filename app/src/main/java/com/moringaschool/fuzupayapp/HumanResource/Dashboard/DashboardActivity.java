d32e94f2fcf9dc287900517596ecb2a34253ed09
allanlimo02 <allan.limo@student.moringaschool.com>
2021-11-17 23:14:32 +0300

    Fixed bottom navigation color

--- app/src/main/java/com/moringaschool/fuzupayapp/HumanResource/Dashboard/DashboardActivity.java
+++ app/src/main/java/com/moringaschool/fuzupayapp/HumanResource/Dashboard/DashboardActivity.java
@@ -17,6 +17,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 import com.moringaschool.fuzupayapp.HumanResource.Fragments.Leave.LeaveActivity;
 import com.moringaschool.fuzupayapp.HumanResource.Fragments.Staff.AddStaffMain;
+import com.moringaschool.fuzupayapp.HumanResource.Fragments.Staff.AllStaffActivity;
 import com.moringaschool.fuzupayapp.HumanResource.Fragments.Staff.DepartmentsFragment;
 import com.moringaschool.fuzupayapp.HumanResource.Fragments.Staff.StaffActivity;
 import com.moringaschool.fuzupayapp.R;
@@ -32,10 +33,6 @@ public class DashboardActivity extends AppCompatActivity  implements View.OnClic
     @BindView(R.id.onleave) TextView onleave;
     @BindView(R.id.approvebutton)  Button approvebutton;
 
-
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,7 +43,6 @@ public class DashboardActivity extends AppCompatActivity  implements View.OnClic
         addstaff.setOnClickListener(this);
         onleave.setOnClickListener(this);
         approvebutton.setOnClickListener(this);
-
 
         bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setSelectedItemId(R.id.nav_home);
@@ -72,40 +68,39 @@ public class DashboardActivity extends AppCompatActivity  implements View.OnClic
         });
     }
 
+
+
     @Override
     public void onClick(View v) {
         if(v==managestaff){
             Intent intent= new Intent(DashboardActivity.this,StaffActivity.class);
             startActivity(intent);
-//            Toast.makeText(DashboardActivity.this,"Manage Staff Coming up Soon",Toast.LENGTH_SHORT).show();
-
         }
         if(v==departments){
-            Intent intent= new Intent(DashboardActivity.this,StaffActivity.class);
+            Intent intent= new Intent(DashboardActivity.this, AllStaffActivity.class);
             startActivity(intent);
             FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-            fragmentTransaction.replace(R.id.frameLayout,new DepartmentsFragment());
+            fragmentTransaction.replace(R.id.ourFrameLayout,new DepartmentsFragment());
             fragmentTransaction.commit();
-
         }
         if(v==addstaff){
             Intent intent= new Intent(DashboardActivity.this,StaffActivity.class);
             startActivity(new Intent(getApplicationContext(), AddStaffMain.class));
             overridePendingTransition(0,0);
             startActivity(intent);
-
         }
         if(v==onleave){
             Intent intent= new Intent(DashboardActivity.this,LeaveActivity.class);
             startActivity(intent);
-
-
         }
         if(v==approvebutton){
             Intent intent=new Intent(DashboardActivity.this,LeaveActivity.class);
             startActivity(intent);
-
         }
-
+    }
+    private void openDepartments(){
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.ourFrameLayout,new DepartmentsFragment());
+        fragmentTransaction.commit();
     }
 }

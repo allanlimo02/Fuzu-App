3b4fc73b3100ca2735dbd5854ef51e90947cb8b5
Esther-Moki <esther.moki@student.moringaschool.com>
2021-11-17 17:03:06 +0300

    made variable declarations for input fields

--- app/src/main/java/com/moringaschool/fuzupayapp/HumanResource/Fragments/Staff/Add_Staff_Input_Fragment.java
+++ app/src/main/java/com/moringaschool/fuzupayapp/HumanResource/Fragments/Staff/Add_Staff_Input_Fragment.java
@@ -1,66 +1,90 @@
 package com.moringaschool.fuzupayapp.HumanResource.Fragments.Staff;
 
-import android.os.Bundle;
+        import android.content.Context;
+        import android.os.Bundle;
 
-import androidx.fragment.app.Fragment;
+        import androidx.fragment.app.Fragment;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+        import android.view.LayoutInflater;
+        import android.view.View;
+        import android.view.ViewGroup;
+        import android.widget.Button;
+        import android.widget.EditText;
+        import android.widget.Toast;
 
-import com.moringaschool.fuzupayapp.R;
+        import com.moringaschool.fuzupayapp.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Add_Staff_Input_Fragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class Add_Staff_Input_Fragment extends Fragment {
+        import butterknife.BindView;
+        import butterknife.ButterKnife;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+public class Add_Staff_Input_Fragment extends Fragment  implements View.OnClickListener{
+    @BindView(R.id.saveDetails)
+    Button mSaveDetailsButton;
+    @BindView(R.id.employeeCode)
+    EditText mEmployeeCodeEditText;
+    @BindView(R.id.surname)    EditText mSurnameEditText;
+    @BindView(R.id.otherName)    EditText mOtherNameEditText;
+    @BindView(R.id.phoneNumber)    EditText mPhoneNumberEditText;
+    @BindView(R.id.email)    EditText mEmailEditText;
+    @BindView(R.id.idPassportNumber)    EditText mIdPassportNumberEditText;
+    @BindView(R.id.nationality)    EditText mNationalityEditText;
+    @BindView(R.id.dateOfBirth)   EditText mDateOfBirthEditText;
+    @BindView(R.id.position)    EditText mPositionEditText;
+    @BindView(R.id.department)    EditText mDepartmentEditText;
+    @BindView(R.id.employmentType)    EditText mEmploymentTypeEditText;
+    @BindView(R.id.employmentDate)    EditText mEmploymentDateEditText;
+    @BindView(R.id.grossSalary)    EditText mGrossSalaryEditText;
+    @BindView(R.id.maritalStatus)    EditText mMaritalStatusEditText;
+    @BindView(R.id.emergencyContact)    EditText mEmergencyContactEditText;
+    @BindView(R.id.emergencyContactPhone)    EditText mEmergencyContactPhoneEditText;
+    @BindView(R.id.bankName)    EditText mBankNameEditText;
+    @BindView(R.id.branch)    EditText mBranchEditText;
+    @BindView(R.id.accountNumber) EditText mAccountNumberEditText;
 
     public Add_Staff_Input_Fragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Add_Staff_Input_Fragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Add_Staff_Input_Fragment newInstance(String param1, String param2) {
-        Add_Staff_Input_Fragment fragment = new Add_Staff_Input_Fragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add__staff__input_, container, false);
+        View view = inflater.inflate(R.layout.fragment_add__staff__input_, container, false);
+        ButterKnife.bind(this,view);
+
+        mSaveDetailsButton.setOnClickListener(this);
+        return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v==mSaveDetailsButton){
+            validator();
+        }
+
+    }
+    public void validator(){
+        String employeeCode = mEmployeeCodeEditText.getText().toString().trim();
+        String surname = mSurnameEditText.getText().toString().trim();
+        String otherName = mOtherNameEditText.getText().toString().trim();
+        String phoneNumber = mPhoneNumberEditText.getText().toString().trim();
+        String email = mEmailEditText.getText().toString().trim();
+        String idPassportNumber = mIdPassportNumberEditText.getText().toString().trim();
+        String nationality = mNationalityEditText.getText().toString().trim();
+        String dateOfBirth = mDateOfBirthEditText.getText().toString().trim();
+        String position = mPositionEditText.getText().toString().trim();
+        String department = mDepartmentEditText.getText().toString().trim();
+        String employmentType = mEmploymentTypeEditText.getText().toString().trim();
+        String employmentDate = mEmploymentDateEditText.getText().toString().trim();
+        String grossSalary = mGrossSalaryEditText.getText().toString().trim();
+        String maritalStatus = mMaritalStatusEditText.getText().toString().trim();
+        String emergencyContact = mEmergencyContactEditText.getText().toString().trim();
+        String emergencyContactPhone = mEmergencyContactPhoneEditText.getText().toString().trim();
+        String bankName = mBankNameEditText.getText().toString().trim();
+        String branch = mBranchEditText.getText().toString().trim();
+        String accountNumber = mAccountNumberEditText.getText().toString().trim();
+
+
     }
 }

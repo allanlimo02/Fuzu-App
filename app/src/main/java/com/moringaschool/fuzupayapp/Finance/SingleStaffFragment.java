5cd45547ad88e56ea5e68ab4cbb7327001483506
allanlimo02 <allan.limo@student.moringaschool.com>
2021-11-23 10:21:24 +0300

    Redone Judy's Work

--- app/src/main/java/com/moringaschool/fuzupayapp/Finance/SingleStaffFragment.java
+++ app/src/main/java/com/moringaschool/fuzupayapp/Finance/SingleStaffFragment.java
@@ -7,22 +7,31 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 
 import com.moringaschool.fuzupayapp.R;
 
-public class SingleStaffFragment extends Fragment {
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
+public class SingleStaffFragment extends Fragment {
+    @BindView(R.id.overView) TextView overViewBtn;
+    @BindView(R.id.deductions) TextView deductionsBtn;
+    @BindView(R.id.benefits) TextView benefitsBtn;
+    @BindView(R.id.flayout) FrameLayout flayout;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
+    public void onStart() {
+        super.onStart();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_staff, container, false);
+        View view=inflater.inflate(R.layout.fragment_staff, container, false);
+        ButterKnife.bind(this, view);
+        return view;
     }
 }

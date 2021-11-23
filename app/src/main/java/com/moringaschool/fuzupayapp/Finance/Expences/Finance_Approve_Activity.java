1f2e6f05bbf7457e11862c0175b43278ac5b5446
allanlimo02 <allan.limo@student.moringaschool.com>
2021-11-23 22:22:27 +0300

    Updated distorted pages

--- app/src/main/java/com/moringaschool/fuzupayapp/Finance/Expences/Finance_Approve_Activity.java
+++ app/src/main/java/com/moringaschool/fuzupayapp/Finance/Expences/Finance_Approve_Activity.java
@@ -50,7 +50,10 @@ public class Finance_Approve_Activity extends AppCompatActivity implements View.
         Pending.setOnClickListener(this);
         payments.setOnClickListener(this);
         completed.setOnClickListener(this);
-
+        Pending.setBackgroundColor(Color.rgb(0,70,115));
+        Pending.setTextColor(Color.WHITE);
+        payments.setTextColor(Color.rgb(0,70,115));
+        completed.setTextColor(Color.rgb(0,70,115));
 
         our_bottom_navigation.setSelectedItemId(R.id.nav_expenses);
         our_bottom_navigation.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {

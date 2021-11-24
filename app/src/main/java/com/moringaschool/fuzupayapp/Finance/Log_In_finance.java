e16fc58caab3f245268fa1a51c44688e4ca86372
allanlimo02 <allan.limo@student.moringaschool.com>
2021-11-24 17:01:52 +0300

    Limo's Updates

--- app/src/main/java/com/moringaschool/fuzupayapp/Finance/Log_In_finance.java
+++ app/src/main/java/com/moringaschool/fuzupayapp/Finance/Log_In_finance.java
@@ -75,7 +75,7 @@ public void loginUser(loginRequest lOginRequest){
 
                                 LoginResponse loginResponse = response.body();
                                 if(loginResponse.getUser().getRole().getId().toString().trim().equals("2")){
-                                startActivity(new Intent(Log_In_finance.this, Dashboard_Finance.class).putExtra("data",loginResponse));
+                                startActivity(new Intent(Log_In_finance.this, DashboardActivity.class).putExtra("data",loginResponse));
                                 finish();
                                 }else
                                 {

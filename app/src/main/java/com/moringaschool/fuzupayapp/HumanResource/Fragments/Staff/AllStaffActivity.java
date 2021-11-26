b03c2bb0f1a09bd01b54e1188fccb18934186cfa
allanlimo02 <allan.limo@student.moringaschool.com>
2021-11-26 04:08:12 +0300

    Fixed loading bug

--- app/src/main/java/com/moringaschool/fuzupayapp/HumanResource/Fragments/Staff/AllStaffActivity.java
+++ app/src/main/java/com/moringaschool/fuzupayapp/HumanResource/Fragments/Staff/AllStaffActivity.java
@@ -11,12 +11,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.text.Layout;
-import android.util.Log;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
@@ -25,14 +19,6 @@ import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
-import android.widget.Spinner;
-import android.widget.Toast;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.navigation.NavigationBarView;
-import com.google.gson.JsonArray;
-import com.moringaschool.fuzupayapp.Department_pojo;
-import com.moringaschool.fuzupayapp.FragmentAdapter.DepartmentAdapter;
 import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -43,17 +29,9 @@ import com.moringaschool.fuzupayapp.APIRequests.StaffApiResources.StaffAdapter;
 import com.moringaschool.fuzupayapp.APIRequests.StaffApiResources.StaffClientClass;
 import com.moringaschool.fuzupayapp.HumanResource.Dashboard.DashboardActivity;
 import com.moringaschool.fuzupayapp.HumanResource.Fragments.Leave.LeaveActivity;
-import com.moringaschool.fuzupayapp.HumanResource.Fragments.Staff.APIclient.staffClient;
-
-import com.moringaschool.fuzupayapp.HumanResource.Fragments.Staff.APIinterface.staffInterface;
 import com.moringaschool.fuzupayapp.R;
 import com.moringaschool.fuzupayapp.SwitchAccount.SwitchLogoutActivity;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
@@ -61,7 +39,7 @@ import butterknife.ButterKnife;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
-import retrofit2.Retrofit;
+
 public class AllStaffActivity extends AppCompatActivity  implements View.OnClickListener, ItemOnclickPosition{
     @BindView(R.id.ourViewStaffHolder)  RecyclerView ourViewStaffHolder;
     @BindView(R.id.bottom_navigation)    BottomNavigationView bottomNavigationView;
@@ -71,7 +49,6 @@ public class AllStaffActivity extends AppCompatActivity  implements View.OnClick
     @BindView(R.id.ourFrameLayout)  FrameLayout ourFrameLayout;
     @BindView(R.id.titleBar) RelativeLayout titleBar;
     @BindView(R.id.imageView5) ImageView logout;
-
     @BindView(R.id.progressBar2) ProgressBar progressBar;
     @BindView(R.id.pleasewait) TextView pleasewait;
     Context context;
@@ -79,25 +56,22 @@ public class AllStaffActivity extends AppCompatActivity  implements View.OnClick
     private ItemOnclickPosition itemOnclickPosition;
 
 
+//    private List list;
+//    private String[] names=new String[]{"Allan Limo","Aron Langat","Esther Moki","Judy Rop","Erick Okumu"};
+//    private String[] position= new String[]{"Manager","C.E.O","Developer","Tester","Production"};
+//    private String[] employmentType= new String[]{" Full time","Contract","Full time","Internship","Internship"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_staff);
         ButterKnife.bind(this);
-        getDetpartMent();
 
         fragmentTwoBtn.setOnClickListener(this);
         fragmentOneBtn.setOnClickListener(this);
         fragmentThreeBtn.setOnClickListener(this);
         logout.setOnClickListener(this);
 
-        // Array adapter feeder
-
-//        DepartmentAdapter adapter=new DepartmentAdapter(this,names,position,employmentType);
-//        ourView.setAdapter(adapter);
-//        ourView.setLayoutManager(new LinearLayoutManager(this));
-        // End of array adapter code
 
         ourViewStaffHolder.setLayoutManager(new LinearLayoutManager(this));
         ourViewStaffHolder.addItemDecoration(new DividerItemDecoration(this,DividerItemDecoration.VERTICAL));
@@ -128,8 +102,9 @@ public class AllStaffActivity extends AppCompatActivity  implements View.OnClick
                 return false;
             }
         });
-      
         //API RESPONSE
+//
+//
     }
     private void fetchAPI(){
         Call<List<StaffResponse>> stafflist= StaffClientClass.staffInterface().getStaff();
@@ -138,7 +113,7 @@ public class AllStaffActivity extends AppCompatActivity  implements View.OnClick
 
             @Override
             public void onResponse(Call<List<StaffResponse>> call, Response<List<StaffResponse>> response) {
-               hideProgressbar();
+                hideProgressbar();
                 if(response.isSuccessful()){
 
                     List<StaffResponse> staffResponses=response.body();
@@ -150,60 +125,6 @@ public class AllStaffActivity extends AppCompatActivity  implements View.OnClick
             public void onFailure(Call<List<StaffResponse>> call, Throwable t) {
                 hideProgressbar();
                 Log.d("Haiwezimake",t.getLocalizedMessage());
-            }
-        });
-    }
-
-    private void getDetpartMent() {
-        staffInterface serviceAPI = staffClient.getDepClient().create(staffInterface.class);
-        serviceAPI.getDepartmentName().enqueue(new Callback<String>() {
-            @Override
-            public void onResponse(Call<String> call, Response<String> response) {
-                Log.i("Response",response.body().toString());
-                if(response.isSuccessful()){
-                    Log.i("Success",response.body().toString());
-                    try{
-                        String getResponse = response.body();
-                        departmentlist=new ArrayList<Department_pojo>();
-                        JSONArray jsonArray = new JSONArray(getResponse);
-                        departmentlist.add(new Department_pojo(-1,"All"));
-                        for(int i=0;i<jsonArray.length();i++){
-                            Department_pojo department_pojo = new Department_pojo();
-                            JSONObject jsonObject = jsonArray.getJSONObject(i);
-                            department_pojo.setId(jsonObject.getInt("id"));
-                            department_pojo.setName(jsonObject.getString("name"));
-                            departmentlist.add(department_pojo);
-                            Log.d("name",department_pojo.getName().toString());
-//                            Log.i("id",department_pojo.getId());
-//                            Toast toast=Toast.makeText(getApplicationContext(),"Hello Javatpoint",Toast.LENGTH_SHORT);
-                        }
-                        for(int i= 0;i<departmentlist.size();i++){
-                            getDepName.add(departmentlist.get(i).getName());
-                        }
-                        ArrayAdapter<String> newDepNameAD = new ArrayAdapter<String>(AllStaffActivity.this, android.R.layout.simple_spinner_item,getDepName);
-                        newDepNameAD.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-                        spinnerDep.setAdapter(newDepNameAD);
-                        spinnerDep.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-                            @Override
-                            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-
-                            }
-
-                            @Override
-                            public void onNothingSelected(AdapterView<?> adapterView) {
-
-                            }
-                        });
-                    }
-                    catch (JSONException ex){
-                        ex.printStackTrace();
-                    }
-                }
-            }
-
-            @Override
-            public void onFailure(Call<String> call, Throwable t) {
-                Toast.makeText(AllStaffActivity.this,t.getMessage(),Toast.LENGTH_LONG).show();
             }
         });
     }
